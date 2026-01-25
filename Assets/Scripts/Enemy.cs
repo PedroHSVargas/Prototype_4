cdc651e2a324ab61f43be99e65f8d73ea30f7d7b
Pedro Vargas <103851885+PedroHSVargas@users.noreply.github.com>
2026-01-25 02:15:26 -0300

    Boss wave added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,11 +6,25 @@ public class Enemy : MonoBehaviour
     private Rigidbody enemyRb;
     private GameObject player;
 
+    public bool isBoss = false;
+
+    public float spawnInterval;
+    private float nextSpawn;
+    
+    public int miniEnemySpawnCount;
+
+    private SpawnManager spawnManager;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         enemyRb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
+
+        if (isBoss)
+        {
+            spawnManager = FindFirstObjectByType<SpawnManager>();
+        }
     }
 
     // Update is called once per frame
@@ -18,6 +32,16 @@ public class Enemy : MonoBehaviour
     {
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed);
+
+        if (isBoss)
+        {
+            if(Time.time > nextSpawn)
+            {
+                nextSpawn = Time.time + spawnInterval;
+                spawnManager.SpawnMiniEnemy(miniEnemySpawnCount);
+            }
+        }
+        
         if (transform.position.y < -10)
         {
             Destroy(gameObject);
